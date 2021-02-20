1091fd840e6702ce64d6a390f06c482bdedf1c33
Sergey Glazunov <31547459+light5551@users.noreply.github.com>
2021-02-20 14:46:54 +0300

    Initial commit

---
+++ launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+echo "This is an empty launch script. Update it to launch your application."
+#roslaunch circle_drive circle_drive.launch
+roslaunch circle_drive circle_drive.launch
